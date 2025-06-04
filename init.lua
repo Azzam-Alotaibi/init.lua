e954077f5eef5c79c949b5fb9bc5528069c426dc
Azzam <azzam.alotaiby@gmail.com>
2025-06-04 22:53:46 +0100

    added highliting after yank
    for some reason it got deleted by mistake

--- init.lua
+++ init.lua
@@ -163,6 +163,14 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Basic Autocommands ]]
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
 vim.api.nvim_create_autocmd('InsertLeave', {
   group = vim.api.nvim_create_augroup('kickstart-auto-save', { clear = true }),
   callback = function()
